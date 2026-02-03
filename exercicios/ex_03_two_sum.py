9b692af5e5b0e1981a38ef969c54537fe784adf2
renzon <renzo@roboflow.com>
2026-02-02 21:35:16 -0300

    Solução linear em tempo e memória para two sum

--- exercicios/ex_03_two_sum.py
+++ exercicios/ex_03_two_sum.py
@@ -7,16 +7,25 @@ Fazer análise de complexidade em tempo e espaço
 
 class Solution:
     """
-    >>> s = Solution()
-    >>> s.twoSum([2, 7, 11, 15] , 9)
-    [0, 1]
 
-    Solução quadrática para tempo de execução
-    Solução constante em tempo de memória
+    >>> s = Solution()
+    >>> s.twoSum([2, 7, 11, 15], 9)
+    [0, 1]
+    >>> s.twoSum([2, 1, 3, 15], 4)
+    [1, 2]
+    >>> s.twoSum([2, 1, 2, 15], 4)
+    [0, 2]
+
+    linear em memória e também em tempo de execução
+
     """
 
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        idx_dct = {}
+        for i, v in enumerate(nums):
+            complement = target - v
+            if complement in idx_dct:
+                return [idx_dct[complement], i]
+
+            idx_dct[v] = i
+
